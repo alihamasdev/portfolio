d1cb210708ea68f38eb5b07d8dae993bf2c3b1bb
Ali Hamas <alihamasdev@gmail.com>
2024-09-23 01:12:30 +0500

    add shad cn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,20 @@
+import colors from "tailwindcss/colors";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+	darkMode: ["class"],
 	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
 		extend: {
-			colors: {}
+			colors: {
+				accent: {
+					purple: colors.purple[500],
+					pink: colors.pink[400]
+				}
+			}
 		}
 	},
-	plugins: []
+	plugins: [require("tailwindcss-animate")]
 };
 
 export default config;
