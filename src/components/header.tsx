97adcd0f12b8d9f0b372478016b5a509dcf01417
Ali Hamas <alihamasdev@gmail.com>
2024-09-24 03:15:37 +0500

    add link component for nextjs top loader compatibility

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,15 @@
 "use client";
 import clsx from "clsx";
+import Link from "next/link";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 import { useTransition } from "@/hooks/transition";
-import { useRouter, usePathname } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
 	const path = usePathname();
-	const router = useRouter();
 	const [isHeader, setIsHeader] = useState(false);
 	return (
 		<>
@@ -29,65 +29,38 @@ export default function Header() {
 						<motion.div
 							{...useTransition({})}
 							className={clsx(path === "/" && "text-gradient font-semibold")}
-							onClick={() => {
-								setIsHeader(false);
-								router.push("/");
-							}}
 						>
-							Home
+							<Link href="/" onClick={() => setIsHeader(false)}>
+								Home
+							</Link>
 						</motion.div>
 						<motion.div
 							{...useTransition({ delay: 0.1 })}
 							className={clsx(path === "/about" && "text-gradient font-semibold")}
-							onClick={() => {
-								setIsHeader(false);
-								router.push("/about");
-							}}
 						>
-							About
+							<Link href="/about" onClick={() => setIsHeader(false)}>
+								About
+							</Link>
 						</motion.div>
 						<motion.div
 							{...useTransition({ delay: 0.2 })}
 							className={clsx(path.includes("/projects") && "text-gradient font-semibold")}
-							onClick={() => {
-								setIsHeader(false);
-								router.push("/projects");
-							}}
 						>
-							Projects
+							<Link href={"/projects"} onClick={() => setIsHeader(false)}>
+								Projects
+							</Link>
 						</motion.div>
 						<motion.div
 							{...useTransition({ delay: 0.3 })}
 							className={clsx(path === "/contact" && "text-gradient font-semibold")}
-							onClick={() => {
-								setIsHeader(false);
-								router.push("/contact");
-							}}
 						>
-							Contact
+							<Link href="/contact" onClick={() => setIsHeader(false)}>
+								Contact
+							</Link>
 						</motion.div>
 					</nav>
 				</header>
 			)}
 		</>
 	);
-}
-
-{
-	/* <header className="fixed left-1/2 top-3 flex h-12 w-[90%] -translate-x-1/2 items-center justify-center rounded-full border border-zinc-800 md:top-7 md:h-14 md:w-fit md:px-20">
-	<nav className="*:decoration-gradient flex w-full items-center justify-evenly gap-x-0 text-sm font-semibold text-zinc-300 md:gap-x-7 md:text-base">
-		<Link href="/" className="">
-			Home
-		</Link>
-		<Link href="/about" className="">
-			About
-		</Link>
-		<Link href="/projects" className="">
-			Projects
-		</Link>
-		<Link href="/contact" className="">
-			Contact
-		</Link>
-	</nav>
-</header> */
 }
