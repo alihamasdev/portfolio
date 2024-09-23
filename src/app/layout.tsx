6314d3fb9b9f8b461ec8db473c01c042bd3b3e48
Ali Hamas <alihamasdev@gmail.com>
2024-09-23 23:14:45 +0500

    make homepage animated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import type { Metadata } from "next";
+
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { AnimatePresence } from "framer-motion";
 
 export const metadata: Metadata = {
 	title: "Developer Portfolio | Ali Hamas",
@@ -15,9 +17,11 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className="bg-black text-zinc-50">
-				<TooltipProvider>
-					<main className="mx-auto w-[90%] max-w-6xl">{children}</main>
-				</TooltipProvider>
+				<AnimatePresence mode="wait">
+					<TooltipProvider>
+						<main className="mx-auto w-[90%] max-w-6xl">{children}</main>
+					</TooltipProvider>
+				</AnimatePresence>
 			</body>
 		</html>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
 import { cloneElement } from "react";
+import { motion } from "framer-motion";
+import { useTransition } from "@/hooks/transition";
 import IconCloud from "@/components/ui/icon-cloud";
 import { faFileLines } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -21,30 +24,42 @@ export default function Home() {
 	return (
 		<section className="flex min-h-dvh w-full flex-col items-center gap-y-10 py-28 md:flex-row">
 			<div className="flex w-full flex-col items-start justify-center md:w-[70%]">
-				<p className="text-base font-medium text-zinc-200 md:text-lg">Hello,</p>
-				<h1 className="text-3xl font-bold leading-tight md:mt-2 md:text-5xl">
+				<motion.p
+					className="text-base font-medium text-zinc-200 md:text-lg"
+					{...useTransition({ delay: 0 })}
+				>
+					Hello,
+				</motion.p>
+				<motion.h1
+					className="text-3xl font-bold leading-tight md:mt-2 md:text-5xl"
+					{...useTransition({ delay: 0.2 })}
+				>
 					I'm <span className="text-gradient">Ali Hamas</span> - Frontend Developer
-				</h1>
-				<p className="mt-2 text-sm text-zinc-300 md:mt-3 md:w-[85%] md:text-base">
+				</motion.h1>
+				<motion.p
+					className="mt-2 text-sm text-zinc-300 md:mt-4 md:w-[85%] md:text-base"
+					{...useTransition({ delay: 0.4 })}
+				>
 					A self-taught developer, who's currently pursuing a Full-Stack development to create
 					stunning user experiences on the front-end, scalable, and secure infrastructure on the
 					backend.
-				</p>
-				<div className="mt-3 flex items-center gap-3 text-zinc-300 md:gap-5">
+				</motion.p>
+				<div className="mt-3 flex items-center gap-3 text-zinc-300 md:mt-5 md:gap-5">
 					{socialLinks.map((item, index) => {
 						return (
-							<a
+							<motion.a
 								key={index}
 								target="_blank"
 								href={item.link}
 								rel="noopener noreferrer"
+								{...useTransition({ delay: 0.6 + 0.2 * index })}
 								className="group-decoration flex flex-wrap items-center gap-1.5 *:transition-colors hover:text-zinc-50 md:hover:[--scale-x:1]"
 							>
 								{cloneElement(item.icon, { className: "h-[18px] md:h-5" })}
-								<span className="decoration-gradient text-sm font-medium md:text-base">
+								<span className="decoration-gradient text-sm font-medium md:text-[15px]">
 									{item.name}
 								</span>
-							</a>
+							</motion.a>
 						);
 					})}
 				</div>
