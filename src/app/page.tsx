a9eb73d305ede880f62d7f2fa68454a77306800d
Ali Hamas <alihamasdev@gmail.com>
2024-09-23 15:56:09 +0500

    add icon cloud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { cloneElement } from "react";
+import IconCloud from "@/components/ui/icon-cloud";
 import { faFileLines } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
@@ -18,31 +19,36 @@ export default function Home() {
 		}
 	];
 	return (
-		<section className="flex min-h-dvh w-full flex-col items-start justify-center">
-			<p className="text-lg font-medium text-zinc-200 md:text-lg">Hello,</p>
-			<h1 className="text-4xl font-bold leading-tight md:mt-2 md:text-5xl">
-				I'm <span className="text-gradient">Ali Hamas</span> - Frontend Developer
-			</h1>
-			<p className="mt-2 text-base text-zinc-200 md:mt-3 md:w-[85%] md:text-lg">
-				A self-taught developer, who's currently pursuing a Full-Stack development to create
-				stunning user experiences on the front-end, scalable, and secure infrastructure on the
-				backend.
-			</p>
-			<div className="mt-3 flex items-center gap-3 text-zinc-200 md:gap-5">
-				{socialLinks.map((item, index) => {
-					return (
-						<a
-							key={index}
-							target="_blank"
-							href={item.link}
-							rel="noopener noreferrer"
-							className="hover-link flex flex-wrap items-center gap-1.5 *:transition-colors hover:text-zinc-50"
-						>
-							{cloneElement(item.icon, { className: "h-[18px] md:h-5" })}
-							<span className="hover-underline text-sm font-medium md:text-base">{item.name}</span>
-						</a>
-					);
-				})}
+		<section className="flex min-h-dvh w-full flex-col items-center gap-y-10 py-28 md:flex-row">
+			<div className="flex w-full flex-col items-start justify-center md:w-[70%]">
+				<p className="text-base font-medium text-zinc-200 md:text-lg">Hello,</p>
+				<h1 className="text-3xl font-bold leading-tight md:mt-2 md:text-5xl">
+					I'm <span className="text-gradient">Ali Hamas</span> - Frontend Developer
+				</h1>
+				<p className="mt-2 text-sm text-zinc-200 md:mt-3 md:w-[85%] md:text-base">
+					A self-taught developer, who's currently pursuing a Full-Stack development to create
+					stunning user experiences on the front-end, scalable, and secure infrastructure on the
+					backend.
+				</p>
+				<div className="mt-3 flex items-center gap-3 text-zinc-200 md:gap-5">
+					{socialLinks.map((item, index) => {
+						return (
+							<a
+								key={index}
+								target="_blank"
+								href={item.link}
+								rel="noopener noreferrer"
+								className="hover-link flex flex-wrap items-center gap-1.5 *:transition-colors hover:text-zinc-50"
+							>
+								{cloneElement(item.icon, { className: "h-[18px] md:h-5" })}
+								<span className="hover-underline text-sm md:text-base">{item.name}</span>
+							</a>
+						);
+					})}
+				</div>
+			</div>
+			<div className="w-[80%] md:w-[30%]">
+				<IconCloud />
 			</div>
 		</section>
 	);
---
+++ src/components/ui/icon-cloud.tsx
@@ -0,0 +1,98 @@
+"use client";
+import { useEffect, useMemo, useState } from "react";
+import { Cloud, fetchSimpleIcons, ICloud, renderSimpleIcon, SimpleIcon } from "react-icon-cloud";
+
+export const cloudProps: Omit<ICloud, "children"> = {
+	containerProps: {
+		style: {
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			width: "100%",
+			paddingTop: 40
+		}
+	},
+	options: {
+		reverse: false,
+		depth: 1,
+		wheelZoom: false,
+		imageScale: 2,
+		activeCursor: "default",
+		tooltip: "native",
+		initial: [0.1, -0.1],
+		clickToFront: 500,
+		tooltipDelay: 0,
+		outlineColour: "#0000",
+		maxSpeed: 0.04,
+		minSpeed: 0.02,
+		dragControl: true
+	},
+	id: "IconCloudId"
+};
+
+export const renderCustomIcon = (icon: SimpleIcon) => {
+	const bgHex = "#080510";
+	const fallbackHex = "#ffffff";
+	const minContrastRatio = 2;
+
+	return renderSimpleIcon({
+		icon,
+		bgHex,
+		fallbackHex,
+		minContrastRatio,
+		size: 42,
+		aProps: {
+			href: undefined,
+			target: undefined,
+			rel: undefined,
+			onClick: (e: any) => e.preventDefault()
+		}
+	});
+};
+
+type IconData = Awaited<ReturnType<typeof fetchSimpleIcons>>;
+
+const skills: string[] = [
+	"javascript",
+	"appwrite",
+	"nextdotjs",
+	"react",
+	"html5",
+	"css3",
+	"tailwindcss",
+	"firebase",
+	"vercel",
+	"git",
+	"github",
+	"figma",
+	"vite",
+	"wordpress",
+	"elementor",
+	"visualstudiocode",
+	"express",
+	"nodejs",
+	"mongodb",
+	"postgresql",
+	"supabase",
+	"typescript"
+];
+export default function IconCloud() {
+	const [data, setData] = useState<IconData | null>(null);
+
+	useEffect(() => {
+		fetchSimpleIcons({ slugs: skills }).then(setData);
+	}, [skills]);
+
+	const renderedIcons = useMemo(() => {
+		if (!data) return null;
+
+		return Object.values(data.simpleIcons).map((icon) => renderCustomIcon(icon));
+	}, [data]);
+
+	return (
+		// @ts-ignore
+		<Cloud {...cloudProps}>
+			<>{renderedIcons}</>
+		</Cloud>
+	);
+}
