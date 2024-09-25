1e078860afb5c06cfdeb4f736974a7b2251c4f8a
Ali Hamas <alihamasdev@gmail.com>
2024-09-26 03:04:29 +0500

    add social links in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,33 +2,40 @@
 import clsx from "clsx";
 import Link from "next/link";
 import { useState } from "react";
+import { Button } from "./ui/button";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
+import { socialLinks } from "@/data/social-links";
 import { useTransition } from "@/hooks/transition";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 
 export default function Header() {
 	const path = usePathname();
 	const [isHeader, setIsHeader] = useState(false);
+
 	return (
 		<>
-			<button
-				className="fixed right-5 top-5 z-20 flex size-10 cursor-pointer items-center justify-center rounded-lg border border-zinc-800 text-zinc-300 transition-all hover:text-zinc-100 md:right-10 md:top-10 md:text-zinc-400 md:hover:ring-2 md:hover:ring-zinc-800"
-				onClick={() => setIsHeader((prevState) => !prevState)}
-			>
-				{isHeader ? (
-					<FontAwesomeIcon icon={faXmark} className="size-5" />
-				) : (
-					<FontAwesomeIcon icon={faBars} className="size-[18px]" />
-				)}
-			</button>
+			<motion.div {...useTransition({})} className="fixed right-5 top-5 z-20 md:right-10 md:top-10">
+				<Button
+					size="icon"
+					variant="outline"
+					onClick={() => setIsHeader((prevState) => !prevState)}
+				>
+					{isHeader ? (
+						<FontAwesomeIcon icon={faXmark} className="size-[17px]" />
+					) : (
+						<FontAwesomeIcon icon={faBars} className="size-[15px]" />
+					)}
+				</Button>
+			</motion.div>
 			{isHeader && (
 				<header className="fixed inset-0 z-10 bg-zinc-950/75 backdrop-blur-md transition-colors duration-300">
-					<nav className="*:decoration-gradient text-zic-300 flex size-full flex-col items-center justify-center gap-y-8 text-xl font-medium md:text-2xl">
+					<nav className="text-zic-300 relative flex size-full flex-col items-center justify-center gap-y-8 text-xl font-medium md:text-2xl">
 						<motion.div
 							{...useTransition({})}
-							className={clsx(path === "/" && "text-gradient font-semibold")}
+							className={clsx("decoration-gradient", path === "/" && "text-gradient font-semibold")}
 						>
 							<Link href="/" onClick={() => setIsHeader(false)}>
 								Home
@@ -36,7 +43,10 @@ export default function Header() {
 						</motion.div>
 						<motion.div
 							{...useTransition({ delay: 0.1 })}
-							className={clsx(path === "/about" && "text-gradient font-semibold")}
+							className={clsx(
+								"decoration-gradient",
+								path === "/about" && "text-gradient font-semibold"
+							)}
 						>
 							<Link href="/about" onClick={() => setIsHeader(false)}>
 								About
@@ -44,7 +54,10 @@ export default function Header() {
 						</motion.div>
 						<motion.div
 							{...useTransition({ delay: 0.2 })}
-							className={clsx(path.includes("/projects") && "text-gradient font-semibold")}
+							className={clsx(
+								"decoration-gradient",
+								path.includes("/projects") && "text-gradient font-semibold"
+							)}
 						>
 							<Link href={"/projects"} onClick={() => setIsHeader(false)}>
 								Projects
@@ -52,12 +65,34 @@ export default function Header() {
 						</motion.div>
 						<motion.div
 							{...useTransition({ delay: 0.3 })}
-							className={clsx(path === "/contact" && "text-gradient font-semibold")}
+							className={clsx(
+								"decoration-gradient",
+								path === "/contact" && "text-gradient font-semibold"
+							)}
 						>
 							<Link href="/contact" onClick={() => setIsHeader(false)}>
 								Contact
 							</Link>
 						</motion.div>
+						<div className="absolute bottom-16 flex items-center gap-x-4">
+							{socialLinks.map((item, index) => {
+								return (
+									<motion.div key={index} {...useTransition({ delay: 0.4 + 0.1 * index })}>
+										<Tooltip>
+											<TooltipTrigger>
+												<a target="_blank" href={item.link} rel="noopener noreferrer">
+													<FontAwesomeIcon
+														icon={item.icon}
+														className="size-5 text-zinc-400 transition-colors hover:text-zinc-50"
+													/>
+												</a>
+											</TooltipTrigger>
+											<TooltipContent>{item.tooltip}</TooltipContent>
+										</Tooltip>
+									</motion.div>
+								);
+							})}
+						</div>
 					</nav>
 				</header>
 			)}
---
+++ src/data/social-links.ts
@@ -0,0 +1,29 @@
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+
+export const socialLinks = [
+	{
+		name: "Email",
+		tooltip: "Contact me at alihamasdev@gmail.com",
+		link: "mailto:alihamasdev@gmail.com",
+		icon: faEnvelope
+	},
+	{
+		name: "Github",
+		tooltip: "Checkout other projects on Github",
+		link: "https://github.com/alihamasdev",
+		icon: faGithub
+	},
+	{
+		name: "LinkedIn",
+		tooltip: "Checkout my LinkedIn",
+		link: "https://linkedin.com/in/alihamasdev",
+		icon: faLinkedin
+	},
+	{
+		name: "Twitter",
+		tooltip: "Follow me on Twitter",
+		link: "https://twitter.com/alihamasdev",
+		icon: faXTwitter
+	}
+];
