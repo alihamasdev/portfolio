ad45cf0a594720e47db018f6e8e31e5208585590
Ali Hamas <alihamasdev@gmail.com>
2024-09-27 00:44:13 +0500

    add projects filter search

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { motion } from "framer-motion";
+import { Input } from "@/components/ui/input";
+import { useState, type ChangeEvent } from "react";
 import { useTransition } from "@/hooks/transition";
 import ProjectCard from "@/components/project-card";
-import { projectsData } from "@/data/projects-data";
+import { projectsData, type ProjectsDataType } from "@/data/projects-data";
 
 export default function Projects() {
+	const [filterProjects, setFilterProjects] = useState<ProjectsDataType[] | []>(projectsData);
+	const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+		const query = e.target.value.toLowerCase();
+		let queryProjects = projectsData.filter((item) => item.title.toLowerCase().includes(query));
+		setFilterProjects(queryProjects);
+	};
 	return (
 		<section className="my-5 md:my-16">
 			<motion.h1
@@ -13,10 +21,19 @@ export default function Projects() {
 			>
 				Projects
 			</motion.h1>
+			<motion.div {...useTransition({ delay: 0.1 })} className="mt-5">
+				<Input type="search" placeholder="Search projects..." onChange={handleSearch} />
+			</motion.div>
 			<div className="mt-6">
-				<div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-					<ProjectCard projects={projectsData} />
-				</div>
+				{filterProjects.length > 0 ? (
+					<div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+						<ProjectCard projects={filterProjects} />
+					</div>
+				) : (
+					<motion.div {...useTransition({})} className="mt-20 text-center text-xl font-medium">
+						No project found
+					</motion.div>
+				)}
 			</div>
 		</section>
 	);
