38791fdbba3d475e728e35cc3d6b5d6fad234c7a
Ali Hamas <alihamasdev@gmail.com>
2024-09-29 03:35:11 +0500

    accepts transition props and fix styling

--- src/components/ui/page-heading.tsx
+++ src/components/ui/page-heading.tsx
@@ -1,11 +1,16 @@
 import { cn } from "@/lib/utils";
 import Transition from "@/lib/transitions";
 import type { HTMLAttributes } from "react";
+import type { TransitionPropsTypes } from "@/lib/transitions";
 
-export default function Heading({ children, className }: HTMLAttributes<"h1">) {
+type PropsTypes = HTMLAttributes<"h1"> & TransitionPropsTypes;
+
+export default function Heading({ children, className, animation, ...props }: PropsTypes) {
 	return (
-		<h1 className={cn("text-4xl font-bold text-zinc-50 md:text-4xl", className)}>
-			<Transition animation={{ name: "fade" }}>{children}</Transition>
+		<h1 className={cn("text-3xl font-bold text-zinc-50 md:text-[42px]", className)}>
+			<Transition animation={{ name: "fade", ...animation }} {...props}>
+				{children}
+			</Transition>
 		</h1>
 	);
 }
