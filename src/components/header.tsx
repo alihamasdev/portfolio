fbe9252e2dc1948437f12500ba68adbbb7f38e5a
Ali Hamas <alihamasdev@gmail.com>
2024-10-01 16:03:43 +0500

    update header component nav_links and enter exit animation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,10 +3,10 @@ import clsx from "clsx";
 import Link from "next/link";
 import { useState } from "react";
 import { Button } from "./ui/button";
-import { motion } from "framer-motion";
+import Transition from "@/lib/transitions";
 import { usePathname } from "next/navigation";
 import { socialLinks } from "@/data/social-links";
-import { useTransition } from "@/hooks/transition";
+import { AnimatePresence, motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
@@ -15,12 +15,22 @@ export default function Header() {
 	const path = usePathname();
 	const [isHeader, setIsHeader] = useState(false);
 
+	const navLinks = [
+		{ name: "Home", link: "/" },
+		{ name: "Projects", link: "/projects" },
+		{ name: "About", link: "/about" },
+		{ name: "Contact", link: "/contact" }
+	];
+
 	return (
-		<>
-			<motion.div {...useTransition({})} className="fixed right-5 top-5 z-20 md:right-10 md:top-10">
+		<header>
+			<Transition
+				animation={{ name: "fade", x: 100, y: 1 }}
+				className="fixed right-5 top-5 z-20 md:right-10 md:top-10"
+			>
 				<Button
 					size="icon"
-					variant="outline"
+					variant="secondary"
 					onClick={() => setIsHeader((prevState) => !prevState)}
 				>
 					{isHeader ? (
@@ -29,73 +39,66 @@ export default function Header() {
 						<FontAwesomeIcon icon={faBars} className="size-[15px]" />
 					)}
 				</Button>
-			</motion.div>
-			{isHeader && (
-				<header className="fixed inset-0 z-10 bg-zinc-950/75 backdrop-blur-md transition-colors duration-300">
-					<nav className="text-zic-300 relative flex size-full flex-col items-center justify-center gap-y-8 text-xl font-medium md:text-2xl">
-						<motion.div
-							{...useTransition({})}
-							className={clsx("decoration-gradient", path === "/" && "text-gradient font-semibold")}
-						>
-							<Link href="/" onClick={() => setIsHeader(false)}>
-								Home
-							</Link>
-						</motion.div>
-						<motion.div
-							{...useTransition({ delay: 0.1 })}
-							className={clsx(
-								"decoration-gradient",
-								path === "/about" && "text-gradient font-semibold"
-							)}
-						>
-							<Link href="/about" onClick={() => setIsHeader(false)}>
-								About
-							</Link>
-						</motion.div>
-						<motion.div
-							{...useTransition({ delay: 0.2 })}
-							className={clsx(
-								"decoration-gradient",
-								path.includes("/projects") && "text-gradient font-semibold"
-							)}
-						>
-							<Link href={"/projects"} onClick={() => setIsHeader(false)}>
-								Projects
-							</Link>
-						</motion.div>
-						<motion.div
-							{...useTransition({ delay: 0.3 })}
-							className={clsx(
-								"decoration-gradient",
-								path === "/contact" && "text-gradient font-semibold"
-							)}
-						>
-							<Link href="/contact" onClick={() => setIsHeader(false)}>
-								Contact
-							</Link>
-						</motion.div>
-						<div className="absolute bottom-16 flex items-center gap-x-4">
-							{socialLinks.map((item, index) => {
-								return (
-									<motion.div key={index} {...useTransition({ delay: 0.4 + 0.1 * index })}>
-										<Tooltip>
-											<TooltipTrigger>
-												<a target="_blank" href={item.link} rel="noopener noreferrer">
-													<FontAwesomeIcon
-														icon={item.icon}
-														className="size-5 text-zinc-400 transition-colors hover:text-zinc-50"
-													/>
-												</a>
-											</TooltipTrigger>
-											<TooltipContent>{item.tooltip}</TooltipContent>
-										</Tooltip>
-									</motion.div>
-								);
-							})}
-						</div>
-					</nav>
-				</header>
-			)}
-		</>
+			</Transition>
+			<AnimatePresence mode="wait">
+				{isHeader && (
+					<motion.section
+						initial={{ y: "100%" }}
+						animate={{ y: 0, transition: { duration: 0.3 } }}
+						exit={{ y: "100%", transition: { duration: 0.3, delay: 0.2 } }}
+						className="fixed inset-0 z-10 border-b border-b-zinc-800 bg-zinc-950/75 backdrop-blur-md transition-colors duration-300"
+					>
+						<nav className="flex min-h-dvh w-full flex-col items-center justify-between py-14">
+							<section />
+							<section className="text-zic-300 flex size-full flex-col items-center justify-center gap-y-8 text-xl font-medium md:text-2xl">
+								{navLinks.map((item, index) => {
+									return (
+										<Transition
+											key={item.name}
+											onClick={() => setIsHeader((prevState) => !prevState)}
+											animation={{ name: "fade", delay: 0.3 + 0.1 * index, exitDelay: 0.1 }}
+											className={clsx(
+												"decoration-gradient",
+												path === item.link && "text-gradient font-semibold",
+												item.link === "/projects" &&
+													path.includes("/projects") &&
+													"text-gradient font-semibold"
+											)}
+										>
+											<Link href={item.link}>{item.name}</Link>
+										</Transition>
+									);
+								})}
+							</section>
+							<section>
+								<div className="flex items-center gap-x-4">
+									{socialLinks.map((item, index) => {
+										return (
+											<Transition
+												key={item.name}
+												className="text-zinc-400 hover:text-zinc-50"
+												animation={{ name: "fade", delay: 0.7 + 0.05 * index, exitDelay: 0.1 }}
+											>
+												<Tooltip>
+													<TooltipTrigger>
+														<a target="_blank" href={item.link} rel="noopener noreferrer">
+															<FontAwesomeIcon
+																icon={item.icon}
+																className="size-5 transition-colors"
+															/>
+														</a>
+													</TooltipTrigger>
+													<TooltipContent sideOffset={12}>{item.tooltip}</TooltipContent>
+												</Tooltip>
+											</Transition>
+										);
+									})}
+								</div>
+							</section>
+						</nav>
+					</motion.section>
+				)}
+			</AnimatePresence>
+		</header>
 	);
 }
