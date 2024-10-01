4d73fab7b7057c1a30e5d3ba82baf38f380d11b3
Ali Hamas <alihamasdev@gmail.com>
2024-10-01 23:34:21 +0500

    update with transition effect

--- src/components/contact-section.tsx
+++ src/components/contact-section.tsx
@@ -22,12 +22,10 @@ export default function ContactSection() {
 						>
 							<AnimatePresence>
 								{hoveredIndex === index && (
-									<motion.div
-										initial={{ opacity: 0 }}
-										layoutId="hoverContactBackground"
-										animate={{ opacity: 1, transition: { duration: 0.15 } }}
-										exit={{ opacity: 0, transition: { duration: 0.15, delay: 0.2 } }}
-										className="absolute inset-0 z-0 rounded-lg border border-zinc-800 bg-zinc-900/90 shadow-lg md:rounded-xl"
+									<Transition
+										layoutId="contactHoverCards"
+										animation={{ name: "hoverFade" }}
+										className="rounded-lg border border-zinc-800 md:rounded-xl"
 									/>
 								)}
 							</AnimatePresence>
