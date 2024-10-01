38f17dfc38d1778d03429cd6481bc41c93795a91
Ali Hamas <alihamasdev@gmail.com>
2024-10-01 23:28:26 +0500

    add hoverFade animation

--- src/lib/transitions.tsx
+++ src/lib/transitions.tsx
@@ -1,37 +1,54 @@
 "use client";
+import { cn } from "./utils";
 import { motion } from "framer-motion";
 import type { HTMLMotionProps, MotionProps } from "framer-motion";
 
 export interface TransitionPropsTypes extends HTMLMotionProps<"div">, MotionProps {
 	animation?: {
-		name: "fade";
+		name?: "fade" | "hoverFade";
 		x?: number | string;
 		y?: number | string;
 		delay?: number;
 		duration?: number;
 		exitDelay?: number;
 		exitDuration?: number;
-		applyAt?: "animate" | "whileInView";
 	};
 }
 
-export default function Transition({ animation, children, ...props }: TransitionPropsTypes) {
-	const y = animation?.y || 70;
-	const x = animation?.x || 0;
-	const delay = animation?.delay || 0;
-	const duration = animation?.duration || 0.3;
-	const exitDelay = animation?.exitDelay || 0.1;
-	const applyAt = animation?.applyAt || "animate";
-	const exitDuration = animation?.exitDuration || duration;
+export default function Transition({
+	animation = {},
+	className,
+	children,
+	initial,
+	animate,
+	exit,
+	...props
+}: TransitionPropsTypes) {
+	const {
+		name,
+		x = 0,
+		y = 70,
+		delay = 0,
+		duration = 0.3,
+		exitDuration = 0.1,
+		exitDelay = duration
+	} = animation;
 
-	if (animation?.name === "fade") {
-		const animateStyles = { opacity: 1, y: 0, x: 0, transition: { delay, duration } };
-
-		props.initial = { opacity: 0, y, x };
-		props.animate = applyAt === "animate" && animateStyles;
-		props.whileInView = applyAt === "whileInView" ? animateStyles : {};
-		props.exit = { opacity: 0, y, x, transition: { delay: exitDelay, duration: exitDuration } };
+	if (name === "fade") {
+		initial = { opacity: 0, y, x };
+		animate = { opacity: 1, y: 0, x: 0, transition: { delay, duration } };
+		exit = { opacity: 0, y, x, transition: { delay: exitDelay, duration: exitDuration } };
+	}
+	if (name === "hoverFade") {
+		initial = { opacity: 0 };
+		animate = { opacity: 1, transition: { duration: 0.15 } };
+		exit = { opacity: 0, transition: { duration: 0.15, delay: 0.2 } };
+		className = cn("absolute inset-0 z-0 size-full bg-zinc-900", className);
 	}
 
-	return <motion.div {...props}>{children}</motion.div>;
+	return (
+		<motion.div initial={initial} animate={animate} exit={exit} className={className} {...props}>
+			{children}
+		</motion.div>
+	);
 }
