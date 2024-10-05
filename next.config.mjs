f2d0336dee483deaf908f4c02bcc13f5e0caf137
Ali Hamas <alihamasdev@gmail.com>
2024-10-05 23:59:35 +0500

    add mdx plugins

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 import createMDX from "@next/mdx";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
 
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"]
 };
 
-const withMDX = createMDX({});
+const withMDX = createMDX({
+	extension: /\.mdx?$/,
+	options: {
+		remarkPlugins: [],
+		rehypePlugins: [rehypeSlug, [rehypePrettyCode, { theme: "one-dark-pro" }]]
+	}
+});
 
 export default withMDX(nextConfig);
