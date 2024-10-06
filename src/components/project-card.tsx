a83cf2f5569a41fccc87a561834ae978940b386d
Ali Hamas <alihamasdev@gmail.com>
2024-10-07 00:01:51 +0500

    delete read more component and add contents in project-card

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -2,11 +2,11 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
-import ReadMore from "./ui/read-more";
 import TechStack from "./ui/tech-stack";
 import Transition from "@/lib/transitions";
 import { AnimatePresence } from "framer-motion";
 import { type ProjectType } from "@/models/types";
+import { ChevronRightIcon } from "@radix-ui/react-icons";
 
 interface PropsTypes {
 	data: ProjectType[];
@@ -49,9 +49,10 @@ export default function ProjectCard({ data }: PropsTypes) {
 						</h2>
 						<div className="mt-1.5 text-xs text-zinc-400 md:text-sm">
 							<p className="line-clamp-4">{project.desc}</p>
-							<span className="decoration-gradient -mt-0.5 [--scale-x:1] lg:[--scale-x:0] lg:group-hover:[--scale-x:1]">
-								<ReadMore iconClass="md:!size-[10px] group-hover:ml-2" />
-							</span>
+							<div className="decoration-gradient -mt-0.5 inline-flex items-center lg:group-hover:[--scale-x:1]">
+								<span>read more</span>
+								<ChevronRightIcon className="mt-0.5 size-4 transition-all duration-300 group-hover:ml-2" />
+							</div>
 						</div>
 					</div>
 				</Link>
